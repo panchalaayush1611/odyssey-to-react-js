acc98786cda6a90c91a2d9530771ed2b9419d1eb
Aayush Panchal <panchalaayush1511@gmail.com>
2026-06-28 23:36:50 +0530

    pushed

--- 01ReactBasics/src/main.jsx
+++ 01ReactBasics/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Coffee from './try.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <h1>this is rendered from main.jsx file</h1>
+    <Coffee />
   </StrictMode>,
 )
---
+++ 01ReactBasics/src/try.jsx
@@ -0,0 +1,8 @@
+function Coffee(){
+    return(
+        <>
+            <h1>Coffee is better!</h1>
+        </>
+    )
+}
+export default Coffee
