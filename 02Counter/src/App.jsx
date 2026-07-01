3d55bdbde139790b2a5ca2413393cc3ef1c8d7b9
Aayush Panchal <panchalaayush1511@gmail.com>
2026-07-01 16:59:55 +0530

    updating

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  let counter = 15;
+
+  const addValue = () => {
+    
+    counter += 1;
+    console.log("Clicked", counter);
+  };
+  const delValue = () => {
+    
+    counter -= 1;
+    console.log("Clicked", counter);
+  };
+
+  return (
+    <>
+      <h1>Chai aur hum</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={delValue}>Decrease Value</button>
+    </>
+  );
+}
+
+export default App;
